b67f2f86722f6c013e059f7ed338de99bbc0ef84
iusearchbtw1337727 <152518983+socute727@users.noreply.github.com>
2024-03-04 21:35:41 +0500

    Add files via upload

--- botitself.py
+++ botitself.py
@@ -1,10 +1,9 @@
-import telebot #тута мы импортируем бота
-from telebot import types #для работы калькулятора
-import requests #тута что бы бот мог угарать с файлами
-import random #тута что бы бот мог угарать с файлами
-import datetime #ограничение по времени
-import json #для хранения токена
-#токен 
+import telebot # Импортируем библиотеку для работы с телеграм ботами
+import requests # Импортируем библиотеку для работы с запросами
+import datetime # Импортируем библиотеку для работы с временными промежутками
+import json # Импортируем библиотеку для работы с конфигами формата json
+
+# Загрузка конфига
 def load_config(filename):
     with open(filename, 'r') as f:
         config = json.load(f)
@@ -15,22 +14,22 @@ TOKEN = config['token']
 
 bot = telebot.TeleBot(TOKEN)
 
-def lmao_megumin():
+# Стартовое сообщение
+def send_megumin_image(message):
     meg_url = 'https://c.tenor.com/z6DsdmHQaxoAAAAd/tenor.gif'
-    return meg_url
-
-#бот стартовое хуярит сообщение
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
     try:
         with open('bot.txt', 'r', encoding='utf-8') as file:
             message_content = file.read()
-        meg_url = lmao_megumin()
         bot.send_document(message.chat.id, meg_url, caption=message_content)
     except Exception as e:
-        bot.reply_to(message, f"бля ну не работает")
+        bot.reply_to(message, f"Произошла ошибка: {e}")
 
-#бот расказывает че умеет
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    send_megumin_image(message)
+
+# Обработчик команды /commandlist
 @bot.message_handler(commands=['commandlist'])
 def send_list(message):
     try:
@@ -38,9 +37,9 @@ def send_list(message):
             message_content = file.read()
         bot.send_message(message.chat.id, message_content)
     except Exception as e:
-        bot.reply_to(message, f"бля ну не работает")
+        bot.reply_to(message, f"Произошла ошибка: {e}")
 
-#faq
+# Обработчик команды /faq
 @bot.message_handler(commands=['faq'])
 def send_faq(message):
     try:
@@ -48,52 +47,29 @@ def send_faq(message):
             message_content = file.read()
         bot.send_message(message.chat.id, message_content)
     except Exception as e:
-        bot.reply_to(message, f"бля ну не работает")
-    
-#гайд по взрывам(хуле нет?)
-@bot.message_handler(commands=['howtoblowup'])
-def send_explosion_info(message):
-    bot.reply_to(message, "Привет! Для того чтобы кастовать магию взрыва, тебе понадобится собрать всю свою магическую энергию в одной точке и затем освободить ее сразу, создав огромный взрыв! Но будь осторожен, кастование магии взрыва требует много сил и опыта. Удачи!")
+        bot.reply_to(message, f"Произошла ошибка: {e}")
 
-#тута из txt файла текст считывает
-@bot.message_handler(commands= ['aboutexplousion'])
-def send_message_from_file(message):
-    try:
-        with open('message.txt', 'r', encoding='utf-8') as file:
-            message_content = file.read()
-        bot.send_message(message.chat.id, message_content)
-    except Exception as e:
-        bot.reply_to(message, f"бля ну не работает")
+# Функция для отправки грустной Мегумин
+def send_sad_megumin(message):
+    img_url = 'https://media1.tenor.com/m/1qjlHimJe2UAAAAC/megumin-crying-megumin.gif'
+    bot.send_document(message.chat.id, img_url, caption="К сожалению, взрывную магию можно использовать лишь раз в день, из-за непомерного расхода маны при использовании.")
 
-#взрыв
-def explosion():
-    file_url = 'https://c.tenor.com/xQV0IM5rFjkAAAAd/tenor.gif'
-    return file_url
-
-#херовина для хранения инфы о том когда ласт тайм была заюзана магия взрыва
+# Для хранения информации о времени последнего использования команды /explode
 last_explode_time = {}
 
-#картинка с грусной Мегумин
-def sad_megumin():
-    img_url = 'https://media1.tenor.com/m/1qjlHimJe2UAAAAC/megumin-crying-megumin.gif'
-    return img_url
-
-def send_sad_megumin(message):
-    img_url = sad_megumin()
-    bot.send_document(message.chat.id, img_url, caption="К сожалению, взрувную магию можно использовать лишь раз в день, из-за непомерного расхода маны при использовании.")
-
-#для проверки, можно ли пользователю использовать команду /explode снова
+# Функция для проверки возможности использования взрыва
 def can_explode(user_id):
     if user_id in last_explode_time:
-        today = datetime.date.today() #чекаем юзал ли юзер(масло масленное) сегодня взрыв
+        today = datetime.date.today()
         return last_explode_time[user_id] != today
     else:
-        return True #если не юзал, разрешаем ему
+        return True
 
-#функшн для обновления времени последнего использования команды /explode
+# Функция для обновления времени последнего использования взрыва
 def update_explode_time(user_id):
     last_explode_time[user_id] = datetime.date.today()
 
+# Обработчик взрыва
 @bot.message_handler(commands=['explode'])
 def explode_user(message):
     try:
@@ -103,32 +79,33 @@ def explode_user(message):
             return
         update_explode_time(user_id)
         bot.reply_to(message, f'Мое имя Мегумин! Величайший гений Клана Алых Магов, кто владеет Взрывной магией! EXPLOUSION!!!')
-        file_url = explosion()
+        file_url = 'https://c.tenor.com/xQV0IM5rFjkAAAAd/tenor.gif'
         bot.send_document(message.chat.id, file_url)
-        #информация о том, кого взорвали
         bot.reply_to(message, f'{user_id} был взорван.')
         explosions_count[user_id] = explosions_count.get(user_id, 0) + 1
-    except IndexError:
-        bot.reply_to(message, 'Кого мне взрывать? Id укажи.')
-
+    except Exception as e:
+        bot.reply_to(message, f'Произошла ошибка: {e}')
+         
+# Для хранения информации о количестве взрывов для каждого пользователя
 explosions_count = {}
 
+# Обработчик команды /expcount
 @bot.message_handler(commands=['expcount'])
 def explosion_count(message):
     try:
-        user_id = message.text.split()[1]
+        user_id = message.from_user.username
         if user_id not in explosions_count:
-            bot.reply_to(message, f'{user_id} еще не кастовал взрывную магию.')
+            bot.reply_to(message, f'@{user_id} еще не кастовал взрывную магию.')
         else:
             count = explosions_count[user_id]
-            bot.reply_to(message, f'{user_id} настоящий алый маг, он использовал взрывную магию уже {count} раз.')
-    except IndexError:
-        bot.reply_to(message, 'A где ID пользователя?')
+            bot.reply_to(message, f'@{user_id} настоящий алый маг, он использовал взрывную магию уже {count} раз.')
+    except Exception as e:
+        bot.reply_to(message, f'Произошла ошибка: {e}')
 
-#рандомные гифки
+# Функция для получения случайной гифки 
 def get_random_konosuba_gif():
     api_key = "edsyceW8ZKcvsPmJaFoTZRhV39ulOGfc"
-    tag = "konosuba"  #тег для поиска гифок
+    tag = "konosuba"
     url = f"https://api.giphy.com/v1/gifs/random?api_key={api_key}&tag={tag}&rating=g"
     response = requests.get(url)
     data = response.json()
@@ -138,19 +115,7 @@ def get_random_konosuba_gif():
     else:
         return None
 
-#получает гиф
-def lain():
-    lain_url = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmNoM3N0MHFhajluaTVtMGoxbjlwOWh4eTdtN2U2b2k5ajJxOWxvZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vP5gXvSXJ2olG/giphy.gif'
-    return lain_url
-#отправляет гиф
-@bot.message_handler(commands=['lain'])
-def hui_vstal(message):
-    lain_url = lain()
-    if lain_url:
-        bot.send_document(message.chat.id, lain_url)
-    else:
-        bot.reply_to(message, "Не удалось получить гифку. Попробуйте позже.")
-
+# Обработчик команды /konosubagif
 @bot.message_handler(commands=['konosubagif'])
 def send_random_gif(message):
     try:
@@ -162,31 +127,32 @@ def send_random_gif(message):
     except Exception as e:
         bot.reply_to(message, f"Ошибка при отправке гифки: {e}")
 
-#жопа
-def get_random_anime_ass_gif():
+# Функция для получения случайной гифки со взрывом
+def get_random_anime_gif():
     api_key = "edsyceW8ZKcvsPmJaFoTZRhV39ulOGfc"
-    tag = "explosion"  #тег для поиска гифок
+    tag = "explosion"
     url = f"https://api.giphy.com/v1/gifs/random?api_key={api_key}&tag={tag}&rating=g"
     response = requests.get(url)
     data = response.json()
     if response.status_code == 200 and 'data' in data:
-        gif_ass = data['data']['images']['original']['url']
-        return gif_ass
+        gif_url = data['data']['images']['original']['url']
+        return gif_url
     else:
         return None
 
+# Обработчик команды /explosions
 @bot.message_handler(commands=['explosions'])
 def send_random_gif(message):
     try:
-        gif_ass = get_random_anime_ass_gif()
-        if gif_ass:
-            bot.send_document(message.chat.id, gif_ass)
+        gif_url = get_random_anime_gif()
+        if gif_url:
+            bot.send_document(message.chat.id, gif_url)
         else:
             bot.reply_to(message, "Не удалось получить гифку :(")
     except Exception as e:
         bot.reply_to(message, f"Ошибка при отправке гифки: {e}")
 
-#калькулятор
+# Обработчик всех текстовых сообщений для калькулятора
 @bot.message_handler(func=lambda message: True)
 def calculate_expression(message):
     try:
@@ -197,4 +163,3 @@ def calculate_expression(message):
         pass
 
 bot.polling()
-#i use arch btw
--- funktest.py
+++ funktest.py
@@ -1,50 +1,180 @@
 import telebot
-
+import requests
 import datetime
+import json
 
-TOKEN = "6463417924:AAEyaWB5ihc2EvtYJLf_PbAXuDbDtdENkTM"
+# Загрузка конфигурации
+def load_config(filename):
+    with open(filename, 'r') as f:
+        config = json.load(f)
+    return config
+
+config = load_config('config.json')
+TOKEN = config['token']
 
 bot = telebot.TeleBot(TOKEN)
 
-#бот стартовое хуярит сообщение
+# Функция для отправки сообщения с изображением Мегумин
+def send_megumin_image(message):
+    meg_url = 'https://c.tenor.com/z6DsdmHQaxoAAAAd/tenor.gif'
+    try:
+        with open('bot.txt', 'r', encoding='utf-8') as file:
+            message_content = file.read()
+        bot.send_document(message.chat.id, meg_url, caption=message_content)
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
+
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.reply_to(message, f"динаху")
+    send_megumin_image(message)
 
-# Словарь для хранения времени последнего использования команды /explode для каждого пользователя
+# Обработчик команды /commandlist
+@bot.message_handler(commands=['commandlist'])
+def send_list(message):
+    try:
+        with open('commandlist.txt', 'r', encoding='utf-8') as file:
+            message_content = file.read()
+        bot.send_message(message.chat.id, message_content)
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
+
+# Обработчик команды /faq
+@bot.message_handler(commands=['faq'])
+def send_faq(message):
+    try:
+        with open('faq.txt', 'r', encoding='utf-8') as file:
+            message_content = file.read()
+        bot.send_message(message.chat.id, message_content)
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
+
+# Обработчик команды /howtoblowup
+@bot.message_handler(commands=['howtoblowup'])
+def send_explosion_info(message):
+    bot.reply_to(message, "Привет! Для того чтобы кастовать магию взрыва, тебе понадобится собрать всю свою магическую энергию в одной точке и затем освободить ее сразу, создав огромный взрыв! Но будь осторожен, кастование магии взрыва требует много сил и опыта. Удачи!")
+
+# Обработчик команды /aboutexplousion
+@bot.message_handler(commands=['aboutexplousion'])
+def send_message_from_file(message):
+    try:
+        with open('message.txt', 'r', encoding='utf-8') as file:
+            message_content = file.read()
+        bot.send_message(message.chat.id, message_content)
+    except Exception as e:
+        bot.reply_to(message, f"Произошла ошибка: {e}")
+
+# Функция для отправки изображения с грустной Мегумин
+def send_sad_megumin(message):
+    img_url = 'https://media1.tenor.com/m/1qjlHimJe2UAAAAC/megumin-crying-megumin.gif'
+    bot.send_document(message.chat.id, img_url, caption="К сожалению, взрывную магию можно использовать лишь раз в день, из-за непомерного расхода маны при использовании.")
+
+# Словарь для хранения информации о времени последнего использования команды /explode
 last_explode_time = {}
 
-# Функция для проверки, можно ли пользователю использовать команду /explode снова
+# Функция для проверки возможности использования команды /explode
 def can_explode(user_id):
     if user_id in last_explode_time:
-        # Проверяем, была ли последняя команда /explode использована сегодня
         today = datetime.date.today()
         return last_explode_time[user_id] != today
     else:
-        # Если пользователь еще не использовал команду, разрешаем ему
         return True
 
 # Функция для обновления времени последнего использования команды /explode
 def update_explode_time(user_id):
     last_explode_time[user_id] = datetime.date.today()
 
+# Обработчик команды /explode
 @bot.message_handler(commands=['explode'])
 def explode_user(message):
     try:
         user_id = message.from_user.id
         if not can_explode(user_id):
-            # Если пользователь уже использовал команду сегодня, сообщаем ему об этом
-            bot.reply_to(message, "Взрувную магию можно использовать лишь раз в день, из-за непомерного расхода маны при использовании.")
+            send_sad_megumin(message)
             return
-        # Обновляем время последнего использования команды /explode для данного пользователя
         update_explode_time(user_id)
         bot.reply_to(message, f'Мое имя Мегумин! Величайший гений Клана Алых Магов, кто владеет Взрывной магией! EXPLOUSION!!!')
-        # Отправляем гифку со взрывом
-        file_url = explosion()
+        file_url = 'https://c.tenor.com/xQV0IM5rFjkAAAAd/tenor.gif'
         bot.send_document(message.chat.id, file_url)
-        # Отправляем информацию о том, кого взорвали
         bot.reply_to(message, f'{user_id} был взорван.')
-    except IndexError:
-        bot.reply_to(message, 'Ты долбаёб? Кого мне взрывать? id укажи.')
+        explosions_count[user_id] = explosions_count.get(user_id, 0) + 1
+    except Exception as e:
+        bot.reply_to(message, f'Произошла ошибка: {e}')
+
+# Словарь для хранения информации о количестве взрывов для каждого пользователя
+explosions_count = {}
+
+# Обработчик команды /expcount
+@bot.message_handler(commands=['expcount'])
+def explosion_count(message):
+    try:
+        user_id = message.from_user.id
+        if user_id not in explosions_count:
+            bot.reply_to(message, f'{user_id} еще не кастовал взрывную магию.')
+        else:
+            count = explosions_count[user_id]
+            bot.reply_to(message, f'{user_id} настоящий алый маг, он использовал взрывную магию уже {count} раз.')
+    except Exception as e:
+        bot.reply_to(message, f'Произошла ошибка: {e}')
+
+# Функция для получения случайной гифки из серии "Konosuba"
+def get_random_konosuba_gif():
+    api_key = "edsyceW8ZKcvsPmJaFoTZRhV39ulOGfc"
+    tag = "konosuba"
+    url = f"https://api.giphy.com/v1/gifs/random?api_key={api_key}&tag={tag}&rating=g"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200 and 'data' in data:
+        gif_url = data['data']['images']['original']['url']
+        return gif_url
+    else:
+        return None
+
+# Обработчик команды /konosubagif
+@bot.message_handler(commands=['konosubagif'])
+def send_random_gif(message):
+    try:
+        gif_url = get_random_konosuba_gif()
+        if gif_url:
+            bot.send_document(message.chat.id, gif_url)
+        else:
+            bot.reply_to(message, "Не удалось получить гифку :(")
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при отправке гифки: {e}")
+
+# Функция для получения случайной гифки с аниме
+def get_random_anime_gif():
+    api_key = "edsyceW8ZKcvsPmJaFoTZRhV39ulOGfc"
+    tag = "explosion"
+    url = f"https://api.giphy.com/v1/gifs/random?api_key={api_key}&tag={tag}&rating=g"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200 and 'data' in data:
+        gif_url = data['data']['images']['original']['url']
+        return gif_url
+    else:
+        return None
+
+# Обработчик команды /explosions
+@bot.message_handler(commands=['explosions'])
+def send_random_gif(message):
+    try:
+        gif_url = get_random_anime_gif()
+        if gif_url:
+            bot.send_document(message.chat.id, gif_url)
+        else:
+            bot.reply_to(message, "Не удалось получить гифку :(")
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при отправке гифки: {e}")
+
+# Обработчик всех текстовых сообщений для калькулятора
+@bot.message_handler(func=lambda message: True)
+def calculate_expression(message):
+    try:
+        text = message.text
+        result = eval(text)
+        bot.reply_to(message, f"Результат: {result}")
+    except Exception as e:
+        pass
 
 bot.polling()
